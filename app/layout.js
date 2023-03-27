bb7da81680ec6b94366cb0267db19cc95bb2a14d
aryan <aaryan.p114@gmail.com>
2023-03-27 23:08:00 +0530

    refactor: unused import removed

--- app/layout.js
+++ app/layout.js
@@ -2,10 +2,6 @@ import '../styles/globals.css';
 
 const RootLayout = ({ children }) => (
   <html lang="en">
-    <head>
-      <link rel="preconnect" href="https://stijndv.com" />
-      <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
-    </head>
     <body className='gradient-dark background-image'>{children}</body>
   </html>
 );
